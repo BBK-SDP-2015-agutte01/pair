2f227647fa977858ffd7ff926fa01620ec82bdb4
Alice <alicegutteridge@gmail.com>
2015-02-25 20:25:17 +0000

    Tests written for getPossibleMoves and makeMove

--- src/test/scala/BoardTest.scala
+++ src/test/scala/BoardTest.scala
@@ -1,11 +1,58 @@
-import org.scalatest.{Matchers, FlatSpec}
+import org.scalatest.{BeforeAndAfterEach, Matchers, FlatSpec}
 
-class BoardTest extends FlatSpec with Matchers {
+class BoardTest extends FlatSpec with Matchers with BeforeAndAfterEach {
 
-//  "A Board" should "return the row number of the top row when makeMove(column) is called, +" +
-//    "in this case column 0 is empty so the result should be zero" in {
-//    val b = Board()
-//    b.test(0) should be (0)
-//  }
+  var board = Board()
+//  val p1 = new Human(YELLOW)
+//  val p2 = new Human(RED)
+
+  override def beforeEach(): Unit = {
+    board = Board()
+  }
+  override def afterEach(): Unit = {
+
+  }
+
+  "A Board" should "return the row number of the top row when makeMove(column) is called, " +
+    "in this case column 0 is empty so the result should be zero" in {
+
+    val move = new Move(YELLOW, 1)
+    board.makeMove(move)
+    board.getTile(5, 1) should be (YELLOW)
+
+  }
+
+  "A Board" should "return an array of Moves of length 7 when getPossibleMoves is called " +
+    "on an empty board" in {
+
+    val arr = Array[Move]()
+    for (i <- 0 until 5) arr :+ new Move(YELLOW, i)
+    board.getPossibleMoves(YELLOW) should be (arr)
+
+  }
+
+  "A Board" should "return an array of Moves of length 6 when getPossibleMoves is called " +
+    "and one column is full" in {
+
+    for (i <- 0 until 5) board.makeMove(new Move(YELLOW, 0))
+
+    val arr = Array[Move]()
+    for (i <- 1 until 5) arr :+ new Move(YELLOW, i)
+
+    board.getPossibleMoves(YELLOW) should be (arr)
+  }
+
+  "A Board" should "return an array of Moves of length 0 when getPossibleMoves is called " +
+    "and all spaces are full" in {
+
+    for (c <- 0 until 5) {
+      for (i <- 0 until 5) board.makeMove(new Move(YELLOW, c))
+    }
+
+    val arr = Array[Move]()
+    board.getPossibleMoves(YELLOW) should be (arr)
+
+  }
+
 
 }
