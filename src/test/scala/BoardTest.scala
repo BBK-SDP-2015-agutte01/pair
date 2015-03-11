efaa066fc77dd57bc10bcd5ef9c1a508932a6ae0
Alice <alicegutteridge@gmail.com>
2015-03-11 17:53:41 +0000

    refactor

--- src/test/scala/BoardTest.scala
+++ src/test/scala/BoardTest.scala
@@ -3,52 +3,30 @@ import org.scalatest.{BeforeAndAfterEach, Matchers, FlatSpec}
 class BoardTest extends FlatSpec with Matchers with BeforeAndAfterEach {
 
   var board = Board()
-  //  val p1 = new Human(YELLOW)
-  //  val p2 = new Human(RED)
 
   override def beforeEach(): Unit = {
     board = Board()
   }
-  override def afterEach(): Unit = {
 
-  }
-
-  "A Board" should "return the row number of the top row when makeMove(column) is called, " +
-    "in this case column 0 is empty so the result should be zero" in {
-
+  "A Board" should "place a Player disc when makeMove(column) is called." in {
     val move = new Move(YELLOW, 1)
     board.makeMove(move)
     board.getTile(5, 1) should be (YELLOW)
-
   }
 
-  "A Board" should "return an array of Moves of length 7 when getPossibleMoves is called " +
-    "on an empty board" in {
-
+  "A getPossibleMoves method" should "return an array of Moves of length 7 when" +
+    "the board is empty" in {
     board.getPossibleMoves(YELLOW).length should be (7)
-
   }
 
-  "A Board" should "return an array of Moves of length 6 when getPossibleMoves is called " +
-    "and one column is full" in {
-
+  it should "return an array of Moves of length 6 when the board has one full column" in {
     board.fillColumn(0)
     board.getPossibleMoves(YELLOW).length should be (6)
-
   }
 
-  "A Board" should "return an array of Moves of length 0 when getPossibleMoves is called " +
-    "and all spaces are full" in {
-
-    for (c <- 0 until 6) {
-      board.fillColumn(c)
-    }
-
+  it should "return an array of Moves of length 0 when the board is full." in {
+    for (c <- 0 until 6) board.fillColumn(c)
     board.getPossibleMoves(YELLOW).length should be (0)
-
   }
-
-
-
 
 }
--- src/test/scala/StateTest.scala
+++ src/test/scala/StateTest.scala
@@ -3,50 +3,34 @@ import org.scalatest.{BeforeAndAfterEach, Matchers, FlatSpec}
 class StateTest extends FlatSpec with Matchers with BeforeAndAfterEach {
 
   var board = Board()
+  val m = new Move(RED, 1)
 
   override def beforeEach(): Unit = {
     board = Board()
   }
-  override def afterEach(): Unit = {
 
-  }
-
-  "A State" should "assign an array of 7 States to var children " +
-    "when initializeChildren is called. " in {
-
-    val m = new Move(RED, 1)
-    board.makeMove(m)
-
+  "A State" should "assign an array of 7 States to var children when initializeChildren is called." in {
     val s = new State(YELLOW, board, m)
     s.initializeChildren()
     s.getChildren.length should be (7)
   }
 
-  "A State" should "assign an array of 6 States to var children when " +
-    "initializeChildren is called and the board has one full column. " in {
-
+  it should "assign an array of 6 States to var children when initializeChildren is called and " +
+    "the board has one full column." in {
     board.fillColumn(0)
-
-    val m = new Move(RED, 2)
-    board.makeMove(m)
-
     val s = new State(YELLOW, board, m)
     s.initializeChildren()
     s.getChildren.length should be (6)
   }
 
-  "A State" should "not be able to add States to var children when  " +
-    "initializeChildren is called and the board is full. " in {
-
+  it should "not be able to add States to var children when initializeChildren is called and " +
+    "the board is full." in {
     for (c <- 0 until 6) {
       board.fillColumn(c)
     }
-
-    val m = new Move(RED, 2)
-
     val s = new State(YELLOW, board, m)
     s.initializeChildren()
-    s.getChildren.length should be (6)
+    s.getChildren.length should be (0)
   }
 
 }
